c793b3bc2f1f96fc5fbb15f9baf4267326c8e7ab
lovroglogar <lovroglogar7@gmail.com>
2024-01-14 16:04:21 +0100

    Fixed conv model 1

--- convolutional_model1.py
+++ convolutional_model1.py
@@ -5,57 +5,6 @@ class ConvolutionalModel1(nn.Module):
         super(ConvolutionalModel1, self).__init__()
 
         self.net = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=32),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=32),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.2),
-
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=64),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=64),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.2),
-
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=128),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=128),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.2),
-
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=256),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=256),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.2),
-
-            nn.Flatten(),
-            nn.Linear(256 * 3 * 3, 256),
-            nn.ReLU(),
-            nn.BatchNorm1d(256),
-            nn.Dropout(p=0.5),
-
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.BatchNorm1d(128),
-            nn.Dropout(p=0.5),
-
-            nn.Linear(128, 7)
-        )
-
-        self.net = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
